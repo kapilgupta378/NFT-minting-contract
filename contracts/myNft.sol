a734268babd881a63a77a05e5eaf001536444239
Kapil <kapil@weblliasto.com>
2023-05-02 16:17:57 +0530

    basic mint function added env added

--- contracts/myNft.sol
+++ contracts/myNft.sol
@@ -4,33 +4,54 @@ pragma solidity ^0.8.9;
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract myNft {
-    uint public unlockTime;
-    address payable public owner;
+contract myNft is ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
 
-    event Withdrawal(uint amount, uint when);
+    constructor() ERC721("Kapil Nft", "KG") {}
 
-    constructor(uint _unlockTime) payable {
-        require(
-            block.timestamp < _unlockTime,
-            "Unlock time should be in the future"
-        );
+    function mintNFT(
+        address recipient,
+        string memory tokenURI
+    ) public onlyOwner returns (uint256) {
+        _tokenIds.increment();
 
-        unlockTime = _unlockTime;
-        owner = payable(msg.sender);
+        uint256 newItemId = _tokenIds.current();
+        _mint(recipient, newItemId);
+
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
     }
 
-    function withdraw() public {
-        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+    // uint public unlockTime;
+    // address payable public owner;
 
-        require(block.timestamp >= unlockTime, "You can't withdraw yet");
-        require(msg.sender == owner, "You aren't the owner");
+    // event Withdrawal(uint amount, uint when);
 
-        emit Withdrawal(address(this).balance, block.timestamp);
+    // constructor(uint _unlockTime) payable {
+    //     require(
+    //         block.timestamp < _unlockTime,
+    //         "Unlock time should be in the future"
+    //     );
 
-        owner.transfer(address(this).balance);
-    }
+    //     unlockTime = _unlockTime;
+    //     owner = payable(msg.sender);
+    // }
+
+    // function withdraw() public {
+    //     // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
+    //     // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+
+    //     require(block.timestamp >= unlockTime, "You can't withdraw yet");
+    //     require(msg.sender == owner, "You aren't the owner");
+
+    //     emit Withdrawal(address(this).balance, block.timestamp);
+
+    //     owner.transfer(address(this).balance);
+    // }
 }
